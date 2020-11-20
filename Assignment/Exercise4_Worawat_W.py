cc1f34b73226755e5597a1c14f049e30075fcaaa
l0vemen0t <72013727+l0vemen0t@users.noreply.github.com>
2020-11-20 14:38:13 +0700

    Create Exercise4_Worawat_W.py
    ส่งการบ้าน Exercise4 Borntodev

---
+++ Assignment/Exercise4_Worawat_W.py
@@ -0,0 +1,17 @@
+'''
+FE = Foundation English
+GB = General Business
+ICS = Introduction to Computer Systems
+CP = Computer Programming
+'''
+
+FE = 90
+GB = 80
+ICS = 85
+CP = 95.5
+
+print("---Your Score---")
+print("Foundation English: ", FE)
+print("General Business: ", GB)
+print("Introduction to Computer Systems: ", ICS)
+print("Computer Programming: ", CP)
