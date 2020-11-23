3cb38bbaf7d5f85ae4bda9014f972d9229f52e1e
l0vemen0t <72013727+l0vemen0t@users.noreply.github.com>
2020-11-23 11:30:10 +0700

    Create Exercise5_1_Worawat_W.py

---
+++ Assignment/Exercise5_1_Worawat_W.py
@@ -0,0 +1,9 @@
+#This is a basic calculator for addition, subtraction, multiplication, and division
+
+a = int(input("1st number: "))
+b = int(input("2st number: "))
+
+print("The addition is", a+b)
+print("The subtraction is", a-b)
+print("The multiplication is", a*b)
+print("The division is", int(a/b))
